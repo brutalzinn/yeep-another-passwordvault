40e91163e83e6800d50c88b5c8f0ed695ee2c710
Roberto Paes <robertocpaes@gmail.com>
2023-12-24 19:12:16 -0300

    wip: add crud operations with sqlite

--- src/api/storage.rs
+++ src/api/storage.rs
@@ -2,20 +2,79 @@
 pub mod storage {
     extern crate rusqlite;
     use rusqlite::{Connection, Result};
-
-    pub fn init() -> Result<()> {
-        let conn = Connection::open_in_memory()?;
-
-    conn.execute(
-        "CREATE TABLE IF NOT EXISTS keypairs (
-            id INTEGER PRIMARY KEY,
-            key TEXT NOT NULL,
-            value TEXT NOT NULL
-        )",
-        (),
-    )?;
-
-    Ok(())
+    #[derive(Debug)]
+    pub struct KeyPair {
+        pub id: i32,
+        pub key: String,
+        pub value: String
     }
+    pub fn create_table(conn: &Connection) -> Result<()> {
+        conn.execute(
+            "CREATE TABLE IF NOT EXISTS keypairs (
+                      id INTEGER PRIMARY KEY,
+                      key TEXT NOT NULL,
+                      value TEXT NOT NULL)",
+            [],
+        )?;
+        Ok(())
+    }
+    
+    pub fn insert_keypair(conn: &Connection, key: &str, value: &str) -> Result<()> {
+        conn.execute(
+            "INSERT INTO keypairs (key, value) VALUES (?1, ?2)",
+            [key, value],
+        )?;
+        Ok(())
+    }
+    
+    // pub fn read_keypair(conn: &Connection, keypair_id: i32) -> Result<Option<KeyPair>> {
+    //     let mut stmt = conn.prepare("SELECT id, key, value FROM keypairs WHERE id = ?1")?;
+    //     let keypair_iter = stmt.query_map(&[&keypair_id], |row| {
+    //         Ok(KeyPair {
+    //             id: row.get(0)?,
+    //             key: row.get(1)?,
+    //             value: row.get(2)?,
+    //         })
+    //     })?;
+    
+    //     for keypair in keypair_iter {
+    //         return Ok(Some(keypair?));
+    //     }
+    
+    //     Ok(None)
+    // }
+
+    pub fn read_keypairs(conn: &Connection) -> Result<Vec<KeyPair>> {
+        let mut stmt = conn.prepare("SELECT id, key, value FROM keypairs")?;
+        let keypair_iter = stmt.query_map([], |row| {
+            Ok(KeyPair {
+                id: row.get(0)?,
+                key: row.get(1)?,
+                value: row.get(2)?,
+            })
+        })?;
+        let mut keypairs : Vec<KeyPair> = Vec::new();
+
+        for keypair in keypair_iter {
+            match keypair {
+                Ok(p) => keypairs.push(p),
+                Err(e) => eprintln!("Error: {e:?}"),
+            }
+        }
+    
+        Ok(keypairs)
+    }
+    
+    // pub fn update_keypair(conn: &Connection, id: i32, new_value: &KeyPair) -> Result<usize> {
+    //     conn.execute(
+    //         "UPDATE keypairs SET name = ?1, value = ?2 WHERE id = ?3",
+    //         [&new_value.key, &new_value.value]
+    //     )
+    // }
+    
+    // pub fn delete_keypair(conn: &Connection, id: i32) -> Result<usize> {
+    //     conn.execute("DELETE FROM keypairs WHERE id = ?1", &[&id])
+    // }
+    
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use api::storage::storage::init;
+use crate::api::storage::storage::{create_table, insert_keypair, read_keypairs};
 use clap::Parser;
+use rusqlite::Connection;
 mod api;
 #[derive(Parser)] 
 #[command(name = "yeep")]
 #[command(bin_name = "yeep")]
 enum Cli {
+    List(ListArgs),
     Add(AddArgs),
     Del(DelArgs),
     Save(SaveArgs)
@@ -16,33 +18,46 @@ struct SaveArgs {
 }
 
 #[derive(clap::Args)]
+#[command(author, version, about, long_about = "List all")]
+struct ListArgs {
+}
+
+#[derive(clap::Args)]
 #[command(author, version, about, long_about = "Add new secret keypair")]
 struct AddArgs {
     #[arg(long)]
-    key: Option<String>,
+    key: String,
 
     #[arg(long)]
-    value: Option<String>,
+    value: String,
 }
 #[derive(clap::Args)]
 #[command(author, version, about, long_about = "Remove a secret")]
 struct DelArgs {
     #[arg(long)]
-    key: Option<String>,
+    id: Option<i32>,
 }
 
 fn main() {
-    let result     =  init();
+    let conn = Connection::open("test.db").unwrap();
+    create_table(&conn).unwrap();
     let args = Cli::parse();
     match args {
         Cli::Add(args) => {
             println!("Add {:?}, {:?}", args.key, args.value);
+            insert_keypair(&conn, &args.key , &args.value).unwrap();   //.unwrap();
         }
         Cli::Del(args) => {
-            println!("Del {:?}", args.key);
+            println!("Del {:?}", args.id);
         }
         Cli::Save(_) => {
             println!("Saving..");
+        }
+        Cli::List(_) => {
+            let keypairs = read_keypairs(&conn).unwrap();
+            for item in keypairs {
+                println!("{} {}", item.id, item.key);
+            }
         }
     }
 }
